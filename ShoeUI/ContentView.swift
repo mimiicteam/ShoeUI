2f1bedae3973720bc630b29e95238ef544d44e89
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-11 23:11:26 +0700

    Create Project

---
+++ ShoeUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ShoeUI
+//
+//  Created by MINH DUC NGUYEN on 11/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ShoeUI/ShoeUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeUIApp.swift
+//  ShoeUI
+//
+//  Created by MINH DUC NGUYEN on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShoeUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
