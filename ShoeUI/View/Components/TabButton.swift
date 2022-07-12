ce5dfa43831649f55f32b34d082374710afb3e5c
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-12 21:07:09 +0700

    Custom Tab Button

--- ShoeUI/View/Components/TabButton.swift
+++ ShoeUI/View/Components/TabButton.swift
@@ -11,24 +11,25 @@ struct TabButton: View {
     @Binding var currentTab: Tab
     var body: some View {
         ForEach(Tab.allCases, id: \.rawValue) { tab in
-            TabButton(tab: tab)
-        }
-    }
-    
-    @ViewBuilder
-    func TabButton(tab: Tab) -> some View {
-        Button {
-            withAnimation {
-                currentTab = tab
+            HStack {
+                Button {
+                    withAnimation {
+                        currentTab = tab
+                    }
+                } label: {
+                    Image(systemName: tab.rawValue)
+                        .resizable()
+                        .renderingMode(.template)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 25, height: 25)
+                        .foregroundColor(tab == currentTab ? .white : Color("BG5"))
+                        .padding(18)
+                        .background(tab == currentTab ? Color("BG5") : .white)
+                        .clipShape(Circle())
+                        .offset(y: tab == currentTab ? -25 : 0)
+                        .frame(maxWidth: .infinity)
+                }
             }
-        } label: {
-            Image(tab.rawValue)
-                .resizable()
-                .renderingMode(.template)
-                .aspectRatio(contentMode: .fit)
-                .frame(width: 25, height: 25)
-                .foregroundColor(tab == currentTab ? Color("BG5") : Color.gray.opacity(0.5))
-                .frame(maxWidth: .infinity)
         }
     }
 }
