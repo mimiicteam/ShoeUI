6b6e8c1047119ffb2ac73766ee77f989e911d935
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-12 22:55:19 +0700

    Segment Tab

--- ShoeUI/View/Components/SegmentButton.swift
+++ ShoeUI/View/Components/SegmentButton.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct SegmentButton: View {
+    @State var image: String
+    @State var title: String
+    
+    @Namespace var animation
+    @EnvironmentObject var appData: AppViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            withAnimation {
+                appData.homeTab = title
+            }
+        } label: {
+            HStack(spacing : 8) {
+                Image(image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 27, height: 27)
+                
+                Text(title)
+                    .font(.system(size: 12.5))
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal, 12)
+            .background(
+                ZStack {
+                    if appData.homeTab == title {
+                        RoundedRectangle(cornerRadius: 15, style: .continuous)
+                            .fill(Color.white)
+                            .matchedGeometryEffect(id: "TAB", in: animation)
+                            .shadow(color: Color.black.opacity(0.04), radius: 5, x: 5, y: 5)
+                    }
+                }
+            )
+        }
     }
 }
 
-struct SegmentButton_Previews: PreviewProvider {
-    static var previews: some View {
-        SegmentButton()
-    }
-}
+//struct SegmentButton_Previews: PreviewProvider {
+//    static var previews: some View {
+//        SegmentButton()
+//    }
+//}
