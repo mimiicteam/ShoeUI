665fc4d7175d18068bb84440ec7828feabd6de36
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-07-12 22:54:53 +0700

    Design Tab

--- ShoeUI/View/Components/TabButton.swift
+++ ShoeUI/View/Components/TabButton.swift
@@ -17,14 +17,14 @@ struct TabButton: View {
                         currentTab = tab
                     }
                 } label: {
-                    Image(systemName: tab.rawValue)
+                    Image(systemName: tab == currentTab ? "\(tab.rawValue).fill" : tab.rawValue)
                         .resizable()
                         .renderingMode(.template)
                         .aspectRatio(contentMode: .fit)
-                        .frame(width: 25, height: 25)
-                        .foregroundColor(tab == currentTab ? .white : Color("BG5"))
+                        .frame(width: tab == currentTab ? 20 : 25, height: tab == currentTab ? 20 : 25)
+                        .foregroundColor(tab == currentTab ? .white : Color("BG"))
                         .padding(18)
-                        .background(tab == currentTab ? Color("BG5") : .white)
+                        .background(tab == currentTab ? Color("BG") : .white)
                         .clipShape(Circle())
                         .offset(y: tab == currentTab ? -25 : 0)
                         .frame(maxWidth: .infinity)
